aac820c52c5167b5cf35be15a5265eefdb73da45
srinaath narasimhan <srinaath2002@gmail.com>
2022-12-01 19:03:54 +0530

    added server and changed structure

---
+++ server/util.py
@@ -0,0 +1,30 @@
+import json
+import pickle
+
+
+__locations = None
+__data_columns = None
+__model = None
+
+def get_location_names():
+    return __locations
+
+def load_saved_artifacts():
+    print("loading saved artifacts...start")
+    global __data_columns
+    global __locations
+    
+    with open("artifacts/banglore_home_price_model.pickle",'rb') as f:
+        __model = pickle.load(f)
+        
+    with open("./artifacts/columns.json",'r') as f:
+        __data_columns = json.load(f)['data_columns']
+        __locations = __data_columns[3:]
+    
+    
+    
+    print("loading saved artifacts ..done")
+
+if __name__ == "__main__":
+    load_saved_artifacts()
+    print(get_location_names())
