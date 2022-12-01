a61bed6db9a765512a75fbec77f2b905e06ee415
srinaath narasimhan <srinaath2002@gmail.com>
2022-12-01 19:21:09 +0530

    flask server done

--- server/server.py
+++ server/server.py
@@ -19,6 +19,19 @@ def get_location_names():
     response.headers.add('Access-Control-Allow-Origin','*')
     return response
 
+@app.route('/predict_home_price',methods=['POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = request.form['bhk']
+    bath = request.form['bath']
+    
+    response = jsonify({
+        'estimated_price ':util.get_estimated_price(location,total_sqft,bhk,bath)
+    })
+    
+
+
 if __name__ == "__main__":
     print("Starting Python Flask Server")
     app.run()
